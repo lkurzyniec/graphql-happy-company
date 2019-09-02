420b61a02cbc11836a7fc4c268662f5a89e972d3
Łukasz Kurzyniec <lkurzyniec@pgs-soft.com>
2019-09-02 19:26:47 +0200

    added Home location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Auth from '@aws-amplify/auth';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isRoot: boolean = window.location.pathname === '/';
+  isLogged: boolean = false;
   title = 'graphql-happy-company';
+
+  constructor() {
+    Auth.currentAuthenticatedUser()
+      .then(() => { this.isLogged = true; })
+      .catch(() => { this.isLogged = false; });
+  }
 }
